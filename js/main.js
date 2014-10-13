0f60dd737bc52cc9917adba8177c3de1e76b18bd
Daniel Loomer <fuzetsu490@gmail.com>
2014-10-12 20:32:29 -0400

    created base javascript file and setup basic API

---
+++ js/main.js
@@ -0,0 +1,51 @@
+(function() {
+
+    var query = window.location;
+
+    var hb = {
+
+        API_URL: 'https://hummingbirdv1.p.mashape.com',
+        API_KEY: 'nr5IdgBU8pmshScE5qxAH92MmFwWp1oqx4mjsnA5igw5vcKlXu',
+
+        // convert to 10 point scoring
+        c10p: function(score) {
+        	return score && parseFloat(score) * 2;
+        },
+
+        sendAPIRequest: function(method, url, data) {
+
+        	var self = this;
+
+            return new Promise(function(resolve, reject) {
+                var xhr = new XMLHttpRequest();
+                xhr.responseType = 'json';
+                xhr.open(method, self.API_URL + url, true);
+                xhr.addEventListener('error', reject);
+                xhr.addEventListener('load', function() {
+                	resolve(xhr.response);
+                });
+                xhr.setRequestHeader('X-Mashape-Key', self.API_KEY);
+                xhr.send(data);
+            });
+
+        },
+
+        getAnimeList: function(username) {
+
+        	var self = this;
+
+            return self
+            	.sendAPIRequest('get', '/users/' + username + '/library', null)
+            	.map(function(entry) {
+            		return {
+            			title: entry.anime.title,
+            			status: entry.status,
+            			rating: self.c10p(entry.rating.value)
+            		};
+            	});
+        }
+
+    };
+
+
+}());
