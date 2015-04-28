086ac59561751fa215161673216a9b8445f188c7
Daniel Loomer <fuzetsu490@gmail.com>
2015-04-27 22:48:36 -0400

    create endpoints in app
    using the hb lib, still missing most, but now has compatibility at least

--- app.js
+++ app.js
@@ -1,19 +1,49 @@
+var hb = require('./lib/hb');
+
 var express = require('express');
 var app = express();
+var server;
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-	res.render('index.jade', {pageTitle: 'Hello World'});
+  res.render('index.jade', {
+    pageTitle: 'Hello World'
+  });
 });
 
-app.get('/compatibility', function(req, res) {
-	var user1 = req.query.user1;
-	var user2 = req.query.user2;
-	if(!user1 || !user2) {
-		return res.end('Please enter the users');
-	}
-	res.send('First user is ' + user1 + ', second user is ' + user2);
+app.get('/compatibility/:type', function(req, res) {
+  var user1 = req.query.user1;
+  var user2 = req.query.user2;
+  var type = req.params.type;
+  var wantsJson = /\.json$/.test(type);
+  if (!user1 || !user2) {
+    return res.status(400).json({
+      status: 400,
+      message: 'You must provide both users in the query string'
+    });
+  } else if (['manga', 'anime'].indexOf(type) == -1) {}
+  if (wantsJson) {
+    type = type.slice(0, -5);
+  }
+  hb.compatibility(user1, user2, type).then(function(compat) {
+    switch (req.accepts(['html', 'json'])) {
+      case 'html':
+        res.render('compat.jade', compat);
+        break;
+      default:
+        res.json(compat);
+        break;
+    }
+  });
 });
 
-var server = app.listen(80);
+app.get('/library/:type/:user', function(req, res) {
+  hb.getList(req.params.user, req.params.type, req.params.status).then(function(list) {
+    res.json(list);
+  }).catch(function(e) {
+    res.end(e);
+  });
+});
+
+server = app.listen(80);
