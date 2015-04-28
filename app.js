1c2b0efa5b79cf391fcbd9ee785cb291664739dc
Daniel Loomer <fuzetsu490@gmail.com>
2015-04-27 23:14:41 -0400

    clean up compat js and finish endpoint

--- app.js
+++ app.js
@@ -13,37 +13,50 @@ app.get('/', function(req, res) {
 });
 
 app.get('/compatibility/:type', function(req, res) {
+  var validTypes = ['manga', 'anime'];
   var user1 = req.query.user1;
   var user2 = req.query.user2;
   var type = req.params.type;
-  var wantsJson = /\.json$/.test(type);
+  var wantsHtml = /\.html$/.test(type);
+
+  if (wantsHtml) {
+    type = type.slice(0, -5);
+  }
+
+  var equal = function(s) {
+    return s === type;
+  };
+
   if (!user1 || !user2) {
     return res.status(400).json({
       status: 400,
       message: 'You must provide both users in the query string'
     });
-  } else if (['manga', 'anime'].indexOf(type) == -1) {}
-  if (wantsJson) {
-    type = type.slice(0, -5);
+  } else if (!validTypes.some(equal)) {
+    return res.status(400).json({
+      status: 400,
+      message: 'The type must be one of the following: ' + validTypes.join(',') + ' instead of ' + type
+    });
   }
+
   hb.compatibility(user1, user2, type).then(function(compat) {
-    switch (req.accepts(['html', 'json'])) {
-      case 'html':
-        res.render('compat.jade', compat);
-        break;
-      default:
-        res.json(compat);
-        break;
+    if(wantsHtml) {
+      res.render('compat.jade', compat);
+    } else {
+      res.json(compat);
     }
   });
+
 });
 
 app.get('/library/:type/:user', function(req, res) {
+
   hb.getList(req.params.user, req.params.type, req.params.status).then(function(list) {
     res.json(list);
   }).catch(function(e) {
     res.end(e);
   });
+
 });
 
 server = app.listen(80);
